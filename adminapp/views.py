acac306b1d0f3cddac0402d03be07c89f0433ac3
shahilkhan123 <shahilkhanarimbra@gmail.com>
2022-05-14 14:17:00 +0530

    added basehtml in adminapp

--- adminapp/views.py
+++ adminapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def branch_home(request):
-    return render(request,'admin/demo.html')
+    return render(request,'admin/partials/base.html')
